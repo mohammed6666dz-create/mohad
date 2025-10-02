b4b880633e757c373f400d25f70884fc98480056
username <mohammed6666dzgmail.com>
2025-10-02 13:53:12 +0100

    حفظ التغييرات الحالية

--- public/hellowodl.cpp
+++ public/hellowodl.cpp
@@ -1,10 +1,8 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
-int main()
-{
 
-
-
-    cout<<"My passion is gaming";
-     return 0;
+int main() {
+    cout << "My passion is gaming" << endl;  // endl لإضافة سطر جديد بعد الطباعة
+    return 0;  // إنهاء البرنامج بنجاح
 }
